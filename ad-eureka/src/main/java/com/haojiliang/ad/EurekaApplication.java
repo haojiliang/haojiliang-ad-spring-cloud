1621650cb4ebf84779117e6d4b5097cb358e36f7
haojiliang <hjl669@qq.com>
2019-03-01 00:31:31 +0800

    init

---
+++ ad-eureka/src/main/java/com/haojiliang/ad/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.haojiliang.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * EurekaApplication
+ *
+ * @author hxx
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
